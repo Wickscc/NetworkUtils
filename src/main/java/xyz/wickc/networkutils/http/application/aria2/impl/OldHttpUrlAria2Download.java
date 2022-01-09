cb993fdbda32ea5ae0046165bb84e76a5b9a030c
Wicks <wickscc@outlook.com>
2022-01-09 11:01:30 +0800

    修复 aria 出现的问题

--- src/main/java/xyz/wickc/networkutils/http/application/aria2/impl/OldHttpUrlAria2Download.java
+++ src/main/java/xyz/wickc/networkutils/http/application/aria2/impl/OldHttpUrlAria2Download.java
@@ -13,12 +13,13 @@ import xyz.wickc.networkutils.http.HttpNetworkUtilsFactory;
 import xyz.wickc.networkutils.http.application.aria2.Aria2Download;
 
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Created on 2020/4/25
- *
+ * <p>
  * 使用 Aria2 来下载 HTTP 协议的资源
- *
+ * <p>
  * 此类已被弃用!
  *
  * @author wicks
@@ -32,8 +33,8 @@ public class OldHttpUrlAria2Download implements Aria2Download {
 
     /**
      * 代理服务器的 URL 地址,格式如下
-     *  [http://][USER:PASSWORD@]HOST[:PORT]
-     *
+     * [http://][USER:PASSWORD@]HOST[:PORT]
+     * <p>
      * 使用 Aria2 来下载时候是否需要用到 Proxy
      * 这里用的 Proxy 是用在 Aria2 服务器上使用的 Proxy!
      * 如果想要在发送数据给Aria2服务器时使用 Proxy 您应该使用 {@link xyz.wickc.networkutils.utils.ConnectionFactory}
@@ -68,50 +69,51 @@ public class OldHttpUrlAria2Download implements Aria2Download {
 
     @Override
     public void downloadUrl(String fileName, String path, String url) {
-        JSONObject sandJson = builderSandJson(fileName,path,url);
+        JSONObject sandJson = builderSandJson(fileName, path, url);
         sandJsonObject(sandJson);
     }
 
     @Override
-    public void downloadUrl(String path,String... urlArr) {
-        JSONObject sandJson = builderSandJson(null,path,urlArr);
+    public void downloadUrl(String path, String... urlArr) {
+        JSONObject sandJson = builderSandJson(null, path, urlArr);
         sandJsonObject(sandJson);
     }
 
     /**
      * 构建一个用于发送请求给 RPC 服务器的 JSONObject
+     *
      * @return json
      */
-    private JSONObject builderSandJson(String fileName, String path, String ...uri){
+    private JSONObject builderSandJson(String fileName, String path, String... uri) {
         JSONObject jsonObject = new JSONObject();
-        jsonObject.put("jsonrpc","2.0");
-        jsonObject.put("method","aria2.addUri");
-        jsonObject.put("id","Aria2AddDownloadUrl,T=" + System.currentTimeMillis());
+        jsonObject.put("jsonrpc", "2.0");
+        jsonObject.put("method", "aria2.addUri");
+        jsonObject.put("id", "Aria2AddDownloadUrl,T=" + System.currentTimeMillis());
 
         JSONArray paramsArray = new JSONArray();
         JSONObject optionJson = new JSONObject();
 
-        if (proxy != null){
-            optionJson.put("http-proxy",proxy.toString());
-            optionJson.put("https-proxy",proxy.toString());
+        if (proxy != null) {
+            optionJson.put("http-proxy", proxy.toString());
+            optionJson.put("https-proxy", proxy.toString());
         }
 
-        if (path != null){
-            optionJson.put("dir",path);
+        if (path != null) {
+            optionJson.put("dir", path);
         }
 
-        if (token != null){
-            paramsArray.add("token:"+token);
+        if (token != null) {
+            paramsArray.add("token:" + token);
         }
 
-        if (fileName != null){
-            optionJson.put("out",fileName);
+        if (fileName != null) {
+            optionJson.put("out", fileName);
         }
 
         paramsArray.add(JSON.toJSON(uri));
         paramsArray.add(optionJson);
 
-        jsonObject.put("params",paramsArray);
+        jsonObject.put("params", paramsArray);
 
         return jsonObject;
     }
@@ -119,16 +121,18 @@ public class OldHttpUrlAria2Download implements Aria2Download {
     /**
      * 发送请求给 Aria2 服务器
      */
-    private void sandJsonObject(JSONObject sandJson){
+    private void sandJsonObject(JSONObject sandJson) {
         HttpNetworkUtils httpNetworkUtils = HttpNetworkUtilsFactory.getHttpNetworkUtils();
-        NetworkRequestData requestData = new NetworkRequestData(aria2ConnectionUrl,requestMethod);
-        requestData.setRequestBodyData(sandJson.toJSONString().getBytes());
+        NetworkRequestData requestData = new NetworkRequestData(aria2ConnectionUrl, requestMethod);
+
+//        设置默认编码格式 UTF-8 防止因为系统默认编码不一致导致的出错!
+        requestData.setRequestBodyData(sandJson.toJSONString().getBytes(StandardCharsets.UTF_8));
 
         NetworkResponseData responseData = httpNetworkUtils.readPage(requestData);
         JSONObject jsonObject = JSON.parseObject(new String(responseData.getRequestBodyData()));
 
         JSONObject errorJson = jsonObject.getJSONObject("error");
-        if (errorJson != null){
+        if (errorJson != null) {
             logger.warn("Aria2 添加任务时出错,返回值为 : " + errorJson.getString("message"));
         }
     }
